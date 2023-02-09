5b9a2ef82a5d6cb6c53f7037770d7cce33d8d15d
Aleksandra1401 <69097147+Aleksandra1401@users.noreply.github.com>
2023-02-09 00:34:07 -0500

    displays time

--- src/hello.java
+++ src/hello.java
@@ -1,23 +1,16 @@
+import java.util.Date;
 
 public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Here ye : important announcement!");
-		
-		System.out.println("Hello World");
-		
-		System.out.println("Goodbye");
-		
-		System.out.println("Hello, Bob!");
-    
-		System.out.println("Hey Alice!");
-		
-		System.out.println("Hi Charlie!");
-		
-		System.out.println("Hello Denise!");
 
-		System.out.println("Hello Ester!");
+		//date object
+        Date current_Date = new Date();
+
+        //display time
+        System.out.println(current_Date);
+
 	}
 
 }
