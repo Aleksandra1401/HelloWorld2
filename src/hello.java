3c8aba9da0f301545e620d6d944d1fe432d34d28
Aleksandra1401 <69097147+Aleksandra1401@users.noreply.github.com>
2023-02-09 00:44:21 -0500

    not sure what happened

--- src/hello.java
+++ src/hello.java
@@ -3,21 +3,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Here ye : important announcement!");
-		
-		System.out.println("Hello World");
-		
-		System.out.println("Goodbye");
-		
-		System.out.println("Hello, Bob!");
-    
-		System.out.println("Hey Alice!");
-		
-		System.out.println("Hi Charlie!");
-		
-		System.out.println("Hello Denise!");
 
-		System.out.println("Hello Ester!");
 	}
 
 }
